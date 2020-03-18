8501331e14c78d78ccea35885ca2173d3b051e13
EC2 Default User <ec2-user@ip-172-31-26-254.us-east-2.compute.internal>
2020-03-18 16:24:22 +0000

    env access token changed

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -2,7 +2,7 @@ require 'facebook/messenger'
 include Facebook::Messenger
 
 Facebook::Messenger::Subscriptions.subscribe(
-  access_token: access_token,
+  access_token: ENV["ACCESS_TOKEN"],
   subscribed_fields: %w[feed mention name]
 )
 
